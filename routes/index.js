64798336f102234fdc2dbae739daa23bd6e383ae
shefaligoel136 <goel136shefali@gmail.com>
2020-12-16 21:16:56 +0530

    add a controller and a router

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+
+const homeController = require('../controllers/home_controller')
+
+// To check if the file is loaded
+console.log("File is loaded");
+router.get('/',homeController.home); //home is accessable over here
+
+module.exports = router;
