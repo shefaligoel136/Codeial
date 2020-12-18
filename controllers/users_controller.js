17afa290502da35d196ac2d52b6d5d5dddf0bee1
shefaligoel136 <goel136shefali@gmail.com>
2020-12-18 12:42:25 +0530

    added sign in and sign up controllers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,15 @@ module.exports.profile = function(request,response){ //exported function profile
         title: "Profile"
     });
 }
+
+module.exports.signIn = function(request,response){ //exported function profile
+    return response.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
+
+module.exports.signUp = function(request,response){ //exported function profile
+    return response.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
