430481d0d41f6d3e0a738b4a2364701dc0626752
shefaligoel136 <goel136shefali@gmail.com>
2021-01-02 11:38:32 +0530

    Create Posts and show respective user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,13 @@
-module.exports.home = function(request,response){ //exported function home
-    console.log(request.cookies);
-    response.cookie('user_id',25);
+const Post = require('../models/post');
 
-    return response.render('home',{
-        title: "Home"
-    });
+module.exports.home = function(request,response){ //exported function home
+    // console.log(request.cookies);
+    // response.cookie('user_id',25);
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return response.render('home',{
+            title: "Codeial|Home",
+            posts : posts
+        });
+    })
 };
