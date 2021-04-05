c741bf963982571320e4d5b3e2c248b63adbc69c
shefaligoel136 <goel136shefali@gmail.com>
2021-04-05 13:55:58 +0530

    made initial socket connection

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,11 @@
 module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer);
 
+    io.sockets.on('connection',function(socket){
+        console.log("New connection received",socket.id);
+
+        socket.on('disconnect',function(){
+            console.log("Socket disconnected");
+        })
+    });
 }
