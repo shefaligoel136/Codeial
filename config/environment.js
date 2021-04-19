f1445fbd31173589e8aeb20de12ad8cefd3a600e
shefaligoel136 <goel136shefali@gmail.com>
2021-04-19 19:11:13 +0530

    Added morgan and rotating file system

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+const logDirectory = path.join(__dirname,'../production_logs');
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream('access.log',{
+    interval : '1d',
+    path : logDirectory
+});
+
+
 const development = {
     name:'development',
     asset_path : './assets',
@@ -16,7 +29,13 @@ const development = {
     google_client_id: "409261034159-mj8q59t25nfgnj95h7iipftn20u1tcm8.apps.googleusercontent.com",
     google_client_secret: "mVQeQhJ9SRj0SqsdoTrg8erk",
     google_callback_url: "http://localhost:8000/users/auth/google/callback",
-    jwt_secret : 'codeial'
+    jwt_secret : 'codeial',
+    morgan : {
+        mode : 'dev',
+        options : {
+            stream : accessLogStream
+        }
+    }
 }
 
 const production = {
@@ -37,7 +56,13 @@ const production = {
     google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
     google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
     google_callback_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
-    jwt_secret : process.env.CODEIAL_JWT_SECRET
+    jwt_secret : process.env.CODEIAL_JWT_SECRET,
+    morgan : {
+        mode : 'combined',
+        options : {
+            stream : accessLogStream
+        }
+    }
 }
 
 // module.exports = development;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-
+const logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
@@ -28,17 +28,19 @@ console.log("Chat server is listening on port 5000");
 const path = require('path');
 
 
+if(env.name == 'development'){
+    app.use(sassMiddleware({
+        // src: './assets/scss',
+        // dest: './assets/css',
+    
+        src : path.join(__dirname,env.asset_path,'scss'),
+        dest : path.join(__dirname,env.asset_path,'css'),
+        debug: true,
+        outputStyle: 'extented',
+        prefix:'/css'
+    }));
+}
 
-app.use(sassMiddleware({
-    // src: './assets/scss',
-    // dest: './assets/css',
-
-    src : path.join(__dirname,env.asset_path,'scss'),
-    dest : path.join(__dirname,env.asset_path,'css'),
-    debug: true,
-    outputStyle: 'extented',
-    prefix:'/css'
-}));
 
 app.use(express.urlencoded());
 
@@ -49,7 +51,9 @@ app.use(express.static(env.asset_path));
 
 
 // make the uplaods path available to the browser
-app.use('/uploads',express.static(__dirname + '/uploads'))
+app.use('/uploads',express.static(__dirname + '/uploads'));
+
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts)
 
